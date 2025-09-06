18620cf9ee0c95d099b7fa54ac976c3603179bd8
Bharath <bharthkamatham143@gmail.com>
2025-09-06 21:03:52 +0530

    Added Leva for tweaking and playing with offset and position.

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,23 +1,41 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { Fragment } from "../shaders/Fragment";
 import { Vertex } from "../shaders/Vertex";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { DoubleSide, TextureLoader } from "three";
 import { useAspect } from "@react-three/drei";
+import { useControls } from "leva";
 
 const Model = () => {
   const mesh = useRef();
   const texture = useLoader(TextureLoader, "/RippleImage.jpg");
+  const { offset, redChannel, blueChannel } = useControls({
+    offset: { value: 0.01, min: 0, max: 0.3, step: 0.01 },
+    redChannel: { value: true },
+    blueChannel: { value: true },
+  });
 
   const scale = useAspect(texture.image.width, texture.image.height, 1);
+
   const uniforms = useRef({
     u_texture: { value: texture },
     u_time: { value: 0 },
+    offset: { value: offset },
+    redChannel: { value: redChannel ? 1 : -1 },
+    blueChannel: { value: blueChannel ? 1 : -1 },
   });
+
+  useEffect(() => {
+    uniforms.current.offset.value = offset;
+    uniforms.current.redChannel.value = redChannel ? 1 : -1;
+    uniforms.current.blueChannel.value = blueChannel ? 1 : -1;
+  }, [offset, redChannel, blueChannel]);
+
   useFrame((state) => {
     const { clock } = state;
-    mesh.current.material.uniforms.u_time.value = clock.getElapsedTime();
+    uniforms.current.u_time.value = clock.getElapsedTime();
   });
+
   return (
     <group>
       <mesh ref={mesh} scale={scale}>
--- src/shaders/Fragment.js
+++ src/shaders/Fragment.js
@@ -2,20 +2,23 @@ const Fragment = `
 varying vec2 vUv;
 uniform sampler2D u_texture;
 uniform float u_time;
+uniform float offset;
+uniform float redChannel;
+uniform float blueChannel;
 
 
 void main() {
     float s=sin(u_time);
-    s = s * 0.05 + 0.1;
+    s = s * offset+ 0.1;
     s *= 0.024;
     vec4 texColor = texture2D(u_texture, vUv);
 
     vec2 redCoord = vUv;
-    redCoord.x -= s;
+    redCoord.x -= s*redChannel;
     float red = texture2D(u_texture, redCoord).r;
 
     vec2 blueCoord = vUv;
-    blueCoord.x += s;
+    blueCoord.x += s*blueChannel;
     float blue = texture2D(u_texture, blueCoord).b;
 
     texColor.r = red;
